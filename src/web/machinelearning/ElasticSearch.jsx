7bd2af2fb6dd9dc960185164758ec7a500760513
Manju <i_manjunathk@microland.com>
2017-11-20 12:57:39 +0530

    backup

--- src/web/machinelearning/ElasticSearch.jsx
+++ src/web/machinelearning/ElasticSearch.jsx
@@ -7,8 +7,8 @@ import CombinedProvider from 'injection/CombinedProvider';
 const { UniversalSearchStore } = CombinedProvider.get('UniversalSearch');
 
 
-const promise = UniversalSearchStore.search('absolute', '*', searchParams, this.props.stream.id, this.PAGE_SIZE,
-  page || 1, 'timestamp', 'asc', undefined, false);
+// const promise = UniversalSearchStore.search('absolute', '*', searchParams, this.props.stream.id, this.PAGE_SIZE,
+//   page || 1, 'timestamp', 'asc', undefined, false);
 const client = new elasticsearch.Client({
   host: '104.198.76.27:9200'
 });
